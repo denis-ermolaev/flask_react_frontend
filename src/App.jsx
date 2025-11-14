87d64f45a1354625435f0b760e07172900f4ab5e
Denis Ermolaev <mrdenis.ppppr@mail.ru>
2025-11-14 23:08:35 +0200

    Добавление navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,27 @@ import "./App.css";
 import UsersDisplay from "./components/UsersDisplay/UsersDisplay";
 import PopUp from "./components/PopUp/PopUp";
 import { Container } from "react-bootstrap";
+import Navbar from "react-bootstrap/Navbar";
 
 function App() {
   return (
     <>
+      <Navbar bg="dark" data-bs-theme="dark" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand>
+            <img
+              alt="React Logo"
+              src="/react.svg"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{" "}
+            Тестовое задание для "Digital агентство Победа"
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
       <Container>
-        <h1 className="my-4">Users</h1>
+        <h1 className="my-4">User Management</h1>
         <div className="my-4">
           <UsersDisplay />
           <PopUp />
